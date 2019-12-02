4fb1fd1554e93aa199d919301bd355779f66d1be
Elen <elcrses@ya.ru>
2019-12-02 20:05:39 +0300

    inputParser: add splitter variable

--- inputParser.js
+++ inputParser.js
@@ -2,13 +2,13 @@
 const fs = require('fs');
 const rawDataFolder = 'data';
 
-const parser = fileName => {
+const parser = (fileName, splitter) => {
     const data = fs.readFileSync(
         __dirname + '/' + rawDataFolder + '/' + fileName,
         {encoding: 'utf-8'}
     );
     
-    return data.toString().split('\n');
+    return data.toString().trim().split(splitter);
 }
 
 module.exports = parser;
